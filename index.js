a56af9ff90f30e136a00fc05615423012ed8b848
Matheus Henrique <mhqsoftware@gmail.com>
2021-12-30 01:52:08 -0300

    Add support to gifs and add custom splash

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import {AppRegistry, useColorScheme} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {AppRegistry, useColorScheme, Image, View} from 'react-native';
 import {ThemeProvider} from 'styled-components/native';
 
+const jobs_splash = require('assets/images/jobs-splash.png');
+import {splashDelay} from '@constants';
 import Routes from './src/routes/Routes';
 import themes from 'styles/themes';
 
@@ -9,10 +11,36 @@ const Jobs = () => {
   const deviceTheme = useColorScheme();
   const theme = themes[deviceTheme] || theme.light;
 
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowSplash(false);
+    }, splashDelay);
+  }, []);
+
   return (
-    <ThemeProvider theme={theme}>
-      <Routes />
-    </ThemeProvider>
+    <>
+      {showSplash ? (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'white',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Image
+            source={jobs_splash}
+            style={{width: 200}}
+            resizeMode={'contain'}
+          />
+        </View>
+      ) : (
+        <ThemeProvider theme={theme}>
+          <Routes />
+        </ThemeProvider>
+      )}
+    </>
   );
 };
 
--- src/functions/constants.ts
+++ src/functions/constants.ts
@@ -3,3 +3,4 @@ import {Dimensions, Platform} from 'react-native';
 export const widthScreen = Dimensions.get('window').width;
 export const heightScreen = Dimensions.get('window').height;
 export const platform = Platform.OS;
+export const splashDelay = 4000;
