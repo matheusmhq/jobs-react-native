ae26c59aa191e7aaf7a9290de2ea793e1e1e9fff
Matheus Henrique <mhqsoftware@gmail.com>
2022-01-01 02:14:27 -0300

    Change splash delay

--- src/functions/constants.ts
+++ src/functions/constants.ts
@@ -3,7 +3,7 @@ import {Dimensions, Platform} from 'react-native';
 export const widthScreen = Dimensions.get('window').width;
 export const heightScreen = Dimensions.get('window').height;
 export const platform = Platform.OS;
-export const splashDelay = 4000;
+export const splashDelay = 1000;
 export const perPage = 20;
 export const msgError =
   'Não foi possível carregar as informações, verifique sua conexão.';
