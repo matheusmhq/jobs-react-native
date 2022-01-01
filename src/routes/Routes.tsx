9c087b10bc9cf049abd422340fefb7723d2796ed
Matheus Henrique <mhqsoftware@gmail.com>
2022-01-01 01:28:13 -0300

    Change to correct param html_url for share job

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -40,8 +40,8 @@ const Routes = () => {
               headerRight: () => (
                 <BtnShare
                   onPress={() => {
-                    const {title, url} = route.params.job;
-                    onShare(`${msgShare} ${title} - ${url}`);
+                    const {title, html_url} = route.params.job;
+                    onShare(`${msgShare} ${title} - ${html_url}`);
                   }}
                   hitSlop={{top: 20, right: 20, bottom: 20, left: 20}}>
                   <Icon name="share" color="black" size={20} />
